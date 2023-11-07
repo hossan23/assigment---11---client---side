186814bdea33cfa23987f8f0009e8a6d46d71c76
hossan23 <imamhossan101@gmail.com>
2023-11-08 01:41:10 +0600

    changed some style of category card in home page

--- src/pages/home/BrowsCategoryCard.jsx
+++ src/pages/home/BrowsCategoryCard.jsx
@@ -10,14 +10,14 @@ const BrowsCategoryCard = ({ data }) => {
    <h2 className="text-2xl font-bold mb-4">{job_title}</h2>
    <hr className="my-4" />
    <p className="text-base text-gray-700 mb-6">{description}</p>
-   <div className="flex flex-col md:flex-row justify-between items-center font-medium">
-    <div className="mb-4 md:mb-0">
-     <div className="bg-blue-100 text-blue-700 font-bold py-2 px-4 rounded-full mb-2">
+   <div className="font-medium space-y-4">
+    <div className="flex justify-between items-center">
+     <div className="bg-blue-100 text-blue-700 font-semibold py-2 px-2 rounded-lg w-fit">
       Price: ${min_price} - ${max_price}
      </div>
-     <div className="bg-green-100 text-green-700 font-bold py-2 px-4 rounded-full">Deadline: {deadline}</div>
+     <div className="bg-green-100 text-green-700 font-semibold py-2 px-2 rounded-lg w-fit">Deadline: {deadline}</div>
     </div>
-    <Link to={`/jobs/${_id}`} className="btn capitalize btn-success">
+    <Link to={`/jobs/${_id}`} className="btn capitalize btn-info w-full">
      Bid Now
     </Link>
    </div>
